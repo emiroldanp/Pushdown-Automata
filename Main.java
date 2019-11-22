797b6d9c4602af32b93d2ace8463dd831a99bfd4
EmilianoR <emiroldanp@hotmail.com>
2019-11-21 23:00:20 -0600

    Fixes to Main File

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 /**
  * Java program to validate string acceptance from a determined grammar via top down parsing.
- * 
- * @author Carlos Maldonado A016
+ * @author Carlos Maldonado A01339011
  * @author Emiliano Roldan A01650141
  */
 
@@ -16,7 +15,6 @@ public class Main {
     public static HashMap<Character, Boolean> lambda = new HashMap<>();
     public static boolean accepted = false;
     /**
-     * 
      * @param args
      * @throws IOException
      * Main class that validates if the string is accepted or not.
@@ -27,9 +25,8 @@ public class Main {
         try {
             file = new File("/Users/emilianoroldan/Documents/GitHub/Top-down Parsing/test3.txt");
             in = new Scanner(file);
-        } catch (FileNotFoundException e1) {
+        } catch (FileNotFoundException e1)
             throw e1;
-        }
 
         String s = "";
         if(in.hasNext()) {
@@ -38,15 +35,12 @@ public class Main {
             s = in.next();
         }
         Character begin = s.charAt(0);
-        System.out.println("The starting point of the tree is: " + begin);
-
         while (in.hasNext()) {
             String productions = in.next();
             Character nonTerm = productions.charAt(0);
             String prod = productions.substring(3);
-            if(!trans.containsKey(nonTerm)) {
+            if(!trans.containsKey(nonTerm))
                 trans.put(nonTerm, new ArrayList<String>());
-            }
             if(prod.equals("lmd")) 
                 lambda.put(nonTerm, true);
             else {
@@ -54,74 +48,69 @@ public class Main {
                     lambda.put(nonTerm, false);
             }
             trans.get(nonTerm).add(prod);
-        }
-        in.close();
+        } in.close();
 
-        System.out.println("Production of the grammar: " + trans);
-        System.out.println("Production of the grammar: " + lambda);
-        Stack<Character> A = new Stack<>();
-        A.add(begin);
+        Stack<Character> stack = new Stack<>();
+        stack.add(begin);
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the string to test");
         String test = scanner.nextLine();
         scanner.close();
         System.out.println("string to test: " + test);
-        verifyString(A, test);
+        verifyString(stack, test);
         if(accepted)
             System.out.println("String is accepted");
         else 
             System.out.println("String is not accepted");
     }
-
     /**
-     * 
-     * @param A
+     * @param stack
      * @param test
      * Function that will check if the given String is produced by the grammar.
      */
-    private static void verifyString(Stack<Character> A, String test){
+    private static void verifyString(Stack<Character> stack, String test){
         if(!accepted) {
-            if(A.size() == 1 && test.equals("")) {
-                if(lambda.containsKey(A.peek()) && lambda.get(A.peek()))
+            if(stack.size() == 1 && test.equals("")) {
+                if(lambda.containsKey(stack.peek()) && lambda.get(stack.peek()))
                     accepted = true;
             }
-            if (test.equals("") && !A.empty()) return;
-            if (!test.equals("") && A.empty()) return;
-            if (A.empty() && test.equals(""))
+            if (test.equals("") && !stack.empty()) return;
+            if (!test.equals("") && stack.empty()) return;
+            if (stack.empty() && test.equals(""))
                 accepted = true;
-            if(!A.empty()) {
-                char ch = A.pop();
+            if(!stack.empty()) {
+                char ch = stack.pop();
                 if (trans.containsKey(ch)) {
                     int count = 0;
                     if(lambda.containsKey(ch) && lambda.get(ch))
                         count++;
                     StringBuilder sBuilder = new StringBuilder();
-                    while (!A.empty()) {
-                        if(lambda.containsKey(A.peek()) && lambda.get(A.peek()))
+                    while (!stack.empty()) {
+                        if(lambda.containsKey(stack.peek()) && lambda.get(stack.peek()))
                             count++;
-                        sBuilder.append(A.pop());
+                        sBuilder.append(stack.pop());
                     }
     
                     if(sBuilder.length() + 1 > test.length() + count)
                         return;
 
                     for (int i = 0; i < trans.get(ch).size(); i++) {
-                        Stack<Character> temp = new Stack<>();
+                        Stack<Character> tempStack = new Stack<>();
                         for (int j = sBuilder.length() - 1; j >= 0; j--)
-                            temp.add(sBuilder.charAt(j));
+                            tempStack.add(sBuilder.charAt(j));
     
                         String s = trans.get(ch).get(i);
     
                         if(!s.equals("lmd")){
                             for (int k = s.length() - 1; k >= 0; k--)
-                                temp.add(s.charAt(k));
+                                tempStack.add(s.charAt(k));
                         }
-                        verifyString(temp, test); 
+                        verifyString(tempStack, test); 
                     }
                 } else {
                     if (ch == test.charAt(0)) {
                         test = test.substring(1);
-                        verifyString(A, test);
+                        verifyString(stack, test);
                     }
                 }
             }
