c28f2e5248260c9dd806442f0bed397a69295e7d
EmilianoR <emiroldanp@hotmail.com>
2019-11-22 11:19:52 -0600

    Implementig GUI 1.0

--- GUI.java
+++ GUI.java
@@ -1,123 +1,151 @@
 
-// import javax.swing.*;
-// import java.awt.*;
-// import java.awt.event.ActionEvent;
-// import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-// public class GUI extends JFrame {
-//     private static final long serialVersionUID = 1L;
-//     private JPanel wrapper;
-//     private JLabel title, insert1, insert2, insert3, txt;
-//     private JTextField nameTxt, stringAutomata, result, stringDisplay;
-//     private JButton verifyString;
+public class GUI extends JFrame {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    // JOptionPane.showMessageDialog(Wrapper, "couln't find the file or null
+    // String");
+    private JPanel wrapper; //This is the pane where all the stuff will be.
+    private JLabel tittle, instr1, instr2, instr3, txt;//This are the variables for the labels in the GUI
+    private JTextField nameTxt, stringAutomata, result, stringDisplay;//This are the text fields where the users can write all the struction nedded for the automarta.
+    private JButton checkString;//This is the boton that will action the automata cheking string method.
 
-//     public GUI(){
-//         /*
-//         *Constructor method that will initialize all the thing to make work the graphic user interface.
-//         */
-//         super("Top-down Parsing");
-//         initComponents();
-//         setLayout(new GridLayout(1, 1));
-//         setDefaultCloseOperation(EXIT_ON_CLOSE);
-//         setSize(900, 300);
-//         setVisible(true);
-//     }
+    public GUI(){
+        /*
+        *
+        *This is the constructor method that will initialize all the thing to make work the graphic user interface.
+        */
+        super("Part 2");//This is the tittle of the window of the GUI
+        initComponents();//This will initialize and add all the variables of the graphic user interface.
+        setLayout(new GridLayout(1, 1));//This is the layout of the window, is a 1 row by 1 column.
+        setDefaultCloseOperation(EXIT_ON_CLOSE);//This will close all the process when pressing the X in the window.
+        setSize(900, 300);//This is the size of the window.
+        setVisible(true);//This will set the window visible to the user
+    }
 
-//     public void initComponents(){
-//         /*
-//         *This method will initialize all the variables and set all the setting needed to display a window to the user.
-//         */
-//         wrapper = new JPanel();
-//         title = new JLabel("Lambda - N D F A");
-//         insert1 = new JLabel("Insert the name of the .txt file");
-//         insert2 = new JLabel("Insert the string");
-//         insert3 = new JLabel("The string was:");
-//         txt = new JLabel(".txt");
-//         nameTxt = new JTextField();
-//         stringAutomata = new JTextField();
-//         result = new JTextField();
-//         stringDisplay = new JTextField();
-//         verifyString = new JButton("Check string");
+    public void initComponents(){
+        /*
+        *
+        *This method will initialize all the variables and set all the setting needed to display a window to the user.
+        *
+        */
+        //The next block of code will initialize all the variables needed to the window.
+        wrapper = new JPanel();
+        tittle = new JLabel("Top-down Parsing");
+        instr1 = new JLabel("Insert the name of the .txt file");
+        instr2 = new JLabel("Insert the string");
+        instr3 = new JLabel("The string was:");
+        txt = new JLabel(".txt");
+        nameTxt = new JTextField();
+        stringAutomata = new JTextField();
+        result = new JTextField();
+        stringDisplay = new JTextField();
+        checkString = new JButton("Check string");
 
-//         title.setFont(new Font("C", Font.BOLD, 18));
-//         title.setHorizontalAlignment(JLabel.CENTER);
-//         insert1.setHorizontalAlignment(JLabel.CENTER);
-//         insert2.setHorizontalAlignment(JLabel.CENTER);
-//         insert3.setHorizontalAlignment(JLabel.RIGHT);
-//         result.setEditable(false);
-//         stringDisplay.setEditable(false);
-//         stringDisplay.setForeground(Color.black);
+        //The block of code will set all the setting needed to make the GUI pretty
+        tittle.setFont(new Font("C", Font.BOLD, 18));
+        tittle.setHorizontalAlignment(JLabel.CENTER);
+        instr1.setHorizontalAlignment(JLabel.CENTER);
+        instr2.setHorizontalAlignment(JLabel.CENTER);
+        instr3.setHorizontalAlignment(JLabel.RIGHT);
+        result.setEditable(false);
+        stringDisplay.setEditable(false);
+        stringDisplay.setForeground(Color.black);
 
-//         verifyString.addActionListener(new checkButtonListener());
-//         wrapper.setLayout(new GridLayout(6, 5));
-//         wrapper.add(new JLabel(""));
-//         wrapper.add(new JLabel(""));
-//         wrapper.add(title);
-//         wrapper.add(new JLabel(""));
-//         wrapper.add(new JLabel(""));
+        //This line of code add the button and action listener to add funcitonality when is click.
+        checkString.addActionListener(new checkButtonListener());
+        //This will add the layout of the wrapper and how to be display.
+        wrapper.setLayout(new GridLayout(6, 5));
 
-//         wrapper.add(new JLabel(""));
-//         wrapper.add(insert1);
-//         wrapper.add(nameTxt);
-//         wrapper.add(txt);
-//         wrapper.add(new JLabel(""));
+        //The rest of lines are the structure of the window row to row.
+        //First row
+        wrapper.add(new JLabel(""));
+        wrapper.add(new JLabel(""));
+        wrapper.add(tittle);
+        wrapper.add(new JLabel(""));
+        wrapper.add(new JLabel(""));
 
-//         wrapper.add(new JLabel(""));
-//         wrapper.add(insert2);
-//         wrapper.add(stringAutomata);
-//         wrapper.add(verifyString);
-//         wrapper.add(new JLabel(""));
+        //Second row
+        wrapper.add(new JLabel(""));
+        wrapper.add(instr1);
+        wrapper.add(nameTxt);
+        wrapper.add(txt);
+        wrapper.add(new JLabel(""));
 
-//         wrapper.add(new JLabel(""));
-//         wrapper.add(new JLabel(""));
-//         wrapper.add(new JLabel(""));
-//         wrapper.add(new JLabel(""));
-//         wrapper.add(new JLabel(""));
+        //third row
+        wrapper.add(new JLabel(""));
+        wrapper.add(instr2);
+        wrapper.add(stringAutomata);
+        wrapper.add(checkString);
+        wrapper.add(new JLabel(""));
 
-//         wrapper.add(new JLabel(""));
-//         wrapper.add(insert3);
-//         wrapper.add(stringDisplay);
-//         wrapper.add(result);
-//         wrapper.add(new JLabel(""));
+        //Fourth row
+        wrapper.add(new JLabel(""));
+        wrapper.add(new JLabel(""));
+        wrapper.add(new JLabel(""));
+        wrapper.add(new JLabel(""));
+        wrapper.add(new JLabel(""));
 
-//         wrapper.add(new JLabel(""));
-//         wrapper.add(new JLabel(""));
-//         wrapper.add(new JLabel(""));
-//         wrapper.add(new JLabel(""));
-//         wrapper.add(new JLabel(""));
+        //Fifth row
+        wrapper.add(new JLabel(""));
+        wrapper.add(instr3);
+        wrapper.add(stringDisplay);
+        wrapper.add(result);
+        wrapper.add(new JLabel(""));
 
-//         add(wrapper);
-//     }
+        //Sixth row
+        wrapper.add(new JLabel(""));
+        wrapper.add(new JLabel(""));
+        wrapper.add(new JLabel(""));
+        wrapper.add(new JLabel(""));
+        wrapper.add(new JLabel(""));
 
-//     public class checkButtonListener implements ActionListener{
-//         public void actionPerformed(ActionEvent e){
-//             /*
-//             *This method will add funcionality to the button when is click.
-//             */
+        //after adding everything we just need to add the wrapper to the GUI.
+        add(wrapper);
+    }
 
-//             Automata auto = new Automata();//This will initialize the object automata where all the magic is being made.
-//             String name = nameTxt.getText();//This will get the name of the .txt file from the text field
-//             String sTest = stringAutomata.getText();//This will get the string to be tested by the automata
-//             boolean res = false;//This is the variable to indicate if the string was accepted by the automata or not
+    public class checkButtonListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            /*
+            *
+            *This method will add funcionality to the button when is click.
+            *
+            */
 
-//             if(!name.equals("") || !sTest.equals("")){
-//                 try {
-//                     res = auto.processString(name, sTest);
-//                 } catch (Exception ex) {
-//                     System.err.println("File is not found");
-//                 }
+            Automata auto = new Automata();//This will initialize the object automata where all the magic is being made.
+            String name = nameTxt.getText();//This will get the name of the .txt file from the text field
+            String sTest = stringAutomata.getText();//This will get the string to be tested by the automata
+            boolean res = false;//This is the variable to indicate if the string was accepted by the automata or not
 
-//                 if(res){
-//                     result.setText("Accepted");
-//                 }else{
-//                     result.setText("Not Accepted");
-//                 }
+            if(!name.equals("") || !sTest.equals("")){
+                //we need a try and catch block because in case the file is not in the project folder.
+                try {
+                    //This will send the information needed to the automata to create the automata and test the stirng.
+                    res = auto.processString(name, sTest);
+                } catch (Exception ex) {
+                    System.err.println("file not found");//Mesasge in case the file is not found.
+                }
 
-//                 stringAutomata.setText("");
-//                 stringDisplay.setText(sTest);
-//             }else{
-//                 JOptionPane.showMessageDialog(wrapper, "both fields must have something");
-//             }
-//         }
-//     }
-// }
+                //this will put the result if the string was acepted by the automata or not.
+                if(res){
+                    result.setText("Accepted");
+                }else{
+                    result.setText("Not Accepted");
+                }
+
+                //This will reset the JTextFields where the user can test another string
+                stringAutomata.setText("");
+                //This will add the string to the JText field to check the result of the automata.
+                stringDisplay.setText(sTest);
+            }else{
+                JOptionPane.showMessageDialog(wrapper, "both fields must have something");
+            }
+        }
+    }
+}
--- Main.java
+++ Main.java
@@ -3,27 +3,22 @@
  * @author Carlos Maldonado A01339011
  * @author Emiliano Roldan A01650141
  */
-
-import java.util.Scanner;
-import java.util.Stack;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 import java.io.*;
-
 public class Main {
     public static HashMap<Character, ArrayList<String>> trans = new HashMap<>();
     public static HashMap<Character, Boolean> lambda = new HashMap<>();
-    public static boolean accepted = false;
     /**
      * @param args
      * @throws IOException Exception thrown if the file is not found.
-     * Main class that validates if the string is accepted or not.
+     * Main class that validates if the string belongs to the grammar or not.
      */
     public static void main(String[] args) throws IOException {
         File file;
         Scanner in;
+        
         try {
-            file = new File("/Users/emilianoroldan/Documents/GitHub/Top-down Parsing/test3.txt");
+            file = new File("/Users/emilianoroldan/Documents/GitHub/Top-down Parsing/test1.txt");
             in = new Scanner(file);
         } catch (FileNotFoundException e1) {
             throw e1;
@@ -34,7 +29,6 @@ public class Main {
             System.out.println("Terminal symbols: " + in.next());
             s = in.next();
         }
-        Character begin = s.charAt(0);
         while (in.hasNext()) {
             String productions = in.next();
             Character nonTerm = productions.charAt(0);
@@ -49,37 +43,37 @@ public class Main {
             }
             trans.get(nonTerm).add(prod);
         } in.close();
-
         System.out.println("Grammar productions: " + trans);
         System.out.println("Grammar productions with lambda: " + lambda);
         Stack<Character> stack = new Stack<>();
+        Character begin = s.charAt(0);
         stack.add(begin);
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the string to test");
         String test = scanner.nextLine();
         scanner.close();
         System.out.println("string to test: " + test);
-        verifyString(stack, test);
-        if(accepted)
-            System.out.println("String is accepted");
+        topDownParsing(stack, test);
+        if(belongs)
+            System.out.println("String belongs to the grammar");
         else 
-            System.out.println("String is not accepted");
+            System.out.println("String does not belongs to the grammar");
     }
     /**
      * @param stack This stack is used to add all the productions of the grammar.
-     * @param test The string that will be checked if is accepted or not.
+     * @param test The string that will be checked if is belongs or not.
      * Function that will check if the given String is produced by the grammar.
      */
-    private static void verifyString(Stack<Character> stack, String test){
-        if(!accepted) {
+    public static boolean belongs = false;
+    private static void topDownParsing(Stack<Character> stack, String test) {
+        if(!belongs) {
             if(stack.size() == 1 && test.equals("")) {
                 if(lambda.containsKey(stack.peek()) && lambda.get(stack.peek()))
-                    accepted = true;
+                    belongs = true;
             }
-            if (test.equals("") && !stack.empty()) return;
-            if (!test.equals("") && stack.empty()) return;
+            if (test.equals("") && !stack.empty()) return;if (!test.equals("") && stack.empty()) return;
             if (stack.empty() && test.equals(""))
-                accepted = true;
+                belongs = true;
             if(!stack.empty()) {
                 char ch = stack.pop();
                 if (trans.containsKey(ch)) {
@@ -92,7 +86,6 @@ public class Main {
                             count++;
                         attacher.append(stack.pop());
                     }
-    
                     if(attacher.length() + 1 > test.length() + count)
                         return;
 
@@ -102,17 +95,16 @@ public class Main {
                             tempStack.add(attacher.charAt(j));
     
                         String s = trans.get(ch).get(i);
-    
                         if(!s.equals("lmd")){
                             for (int k = s.length() - 1; k >= 0; k--)
                                 tempStack.add(s.charAt(k));
                         }
-                        verifyString(tempStack, test); 
+                        topDownParsing(tempStack, test); 
                     }
                 } else {
                     if (ch == test.charAt(0)) {
                         test = test.substring(1);
-                        verifyString(stack, test);
+                        topDownParsing(stack, test);
                     }
                 }
             }
